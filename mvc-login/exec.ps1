038b5ca584930e6e097e30837c0736a205378c13
Steve Xu <stevexu01@gmail.com>
2020-10-06 10:22:45 -0400

    Cleanup

--- mvc-login/exec.ps1
+++ mvc-login/exec.ps1
@@ -1,2 +1,2 @@
-docker build -t auth0-samples/auth0-spring-boot-mvc-login .
-docker run -p 3000:3000 -it auth0-samples/auth0-spring-boot-mvc-login
+docker build -t <container> .
+docker run -p 3000:3000 -it <container>
